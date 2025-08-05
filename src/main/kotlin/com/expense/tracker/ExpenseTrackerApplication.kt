02c6f32e59a7eda1b35fdb264310ef9f62a7ea1b
Madhu Kommula <madhu.544.iiit@gmail.com>
2025-08-05 17:10:31 +0200

    fix application startup

--- src/main/kotlin/com/expense/tracker/ExpenseTrackerApplication.kt
+++ src/main/kotlin/com/expense/tracker/ExpenseTrackerApplication.kt
@@ -4,9 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ExpenseTrackerApplication {
+class ExpenseTrackerApplication
 
     fun main(args: Array<String>) {
         runApplication<ExpenseTrackerApplication>(*args)
     }
-}
